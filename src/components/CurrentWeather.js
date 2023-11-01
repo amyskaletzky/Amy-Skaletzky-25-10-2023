8d53d6d7398a80be683f6e1aaaede67429b4f566
Amy Skaletzky <amy.skaletzky@gmail.com>
2023-11-01 15:48:41 +0200

    finished with current weather

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -5,6 +5,8 @@ import { getDayOfWeek, getDateNow, getTime } from "../helpers/dayDate";
 import beforeFavourited from '../assets/preFavouriteStar.svg';
 import favouritedStar from '../assets/favouriteStar.svg';
 import uvIndex from '../assets/1.svg';
+import sunrise from '../assets/sunrise.svg'
+import sunset from '../assets/sunset.svg'
 
 const CurrentWeather = (props) => {
     const dispatch = useDispatch()
@@ -73,27 +75,37 @@ const CurrentWeather = (props) => {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col h-full bg-white w-1/2 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-40 border border-white border-opacity-60 gap-8">
-                    <div className="flex justify-between px-4 py-2">
-                        <div className="flex flex-col items-center">
+                <div id="extra-info" className="flex flex-col h-full bg-white w-1/2 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-40 border border-white border-opacity-60">
+                    <div className="flex justify-between px-8 py-2 h-1/2">
+                        <div className="flex flex-col items-center justify-center ">
                             <p className="text-3xl">UV-Index</p>
-                            <img src={uvIndex} className="w-20" />
-                            <p className="text-3xl">{cityCurrentWeather[0].UVIndex} - {cityCurrentWeather[0].UVIndexText}</p>
+                            <img src={uvIndex} className="w-12" />
+                            <p className="text-2xl">{cityCurrentWeather[0].UVIndex} - {cityCurrentWeather[0].UVIndexText}</p>
                         </div>
-                        
-                        <div className="flex flex-col justify-center gap-4">
+                        <div className="border-l border-gray-500 border-opacity-40 h-full flex"></div>
+                        <div className="flex flex-col justify-center  gap-4">
                             <p className="text-3xl">Wind Speed</p>
                             <p className="text-2xl">{cityCurrentWeather[0].Wind.Speed.Metric.Value + ' ' + cityCurrentWeather[0].Wind.Speed.Metric.Unit}</p>
                         </div>
-                        <div className="flex flex-col justify-center gap-4">
+                        <div className="border-l border-gray-500 border-opacity-40 h-full"></div>
+
+                        <div className="flex flex-col justify-center items-center gap-4 ">
                             <p className="text-3xl">Humidity</p>
                             <p className="text-2xl">{cityCurrentWeather[0].RelativeHumidity + '%'}</p>
                         </div>
-
                     </div>
-                    <div className="flex">
-                        <p className="text-xl">{getTime(sunForecast.DailyForecasts[0].Sun.Set)}</p>
-                        <p className="text-xl">{getTime(sunForecast.DailyForecasts[0].Sun.Rise)}</p>
+                    {/* whyyy??????? */}
+                    <div className="border-t border-gray-500 border-opacity-40 w-full mx-auto px-8"></div>
+                    <div className="flex h-1/2 items-center justify-center gap-7">
+                        <div>
+                            <img src={sunrise} className="w-20" />
+                            <p className="text-xl">{getTime(sunForecast.DailyForecasts[0].Sun.Set)}</p>
+                        </div>
+                        <div className="border-t border-gray-500 border-opacity-40 w-1/3 "></div>
+                        <div>
+                            <img src={sunset} className="w-20" />
+                            <p className="text-xl">{getTime(sunForecast.DailyForecasts[0].Sun.Rise)}</p>
+                        </div>
                     </div>
                 </div>
             </div>
