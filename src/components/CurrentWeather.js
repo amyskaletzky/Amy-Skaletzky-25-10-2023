44521e321941668864d5c2222d5333d6b15fa8bf
Amy Skaletzky <amy.skaletzky@gmail.com>
2023-11-01 15:21:51 +0200

    almost done with displaying current weather

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { autoCompleteCountryWeather, getCurrentWeather, getCurrentLocation, getFiveDayForecast, clearCities, getSunriseSunset } from "../actions";
 import { getDayOfWeek, getDateNow, getTime } from "../helpers/dayDate";
-import beforeFavourited from '../assets/preFavouriteStar.svg'
-import favouritedStar from '../assets/favouriteStar.svg'
+import beforeFavourited from '../assets/preFavouriteStar.svg';
+import favouritedStar from '../assets/favouriteStar.svg';
+import uvIndex from '../assets/1.svg';
 
 const CurrentWeather = (props) => {
     const dispatch = useDispatch()
@@ -54,28 +55,46 @@ const CurrentWeather = (props) => {
     return (
         (cityCurrentWeather?.length > 0 && sunForecast) ?
             <div className="flex h-1/2 gap-4" >
-                <div className="flex w-1/2 bg-white rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-25 border border-white border-opacity-60 justify-between">
-                    <div className="flex flex-col gap-4 px-8 py-1">
+                <div className="flex w-1/2 bg-white rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-40 border border-white border-opacity-60 justify-between">
+                    <div className="flex flex-col gap-4 px-8 py-2 text-left">
                         <img src={beforeFavourited} className="w-10 " />
-                        <img src={require('../assets/' + cityCurrentWeather[0].WeatherIcon + '.svg')} className="w-32 self-center" />
+                        <img src={require('../assets/' + cityCurrentWeather[0].WeatherIcon + '.svg')} className="w-28" />
                         <div className="flex flex-col gap-4">
-                            <p className="text-5xl">{cityCurrentWeather[0].Temperature.Metric.Value} {cityCurrentWeather[0].Temperature.Metric.Unit}</p>
-                            <p className="text-4xl">{cityCurrentWeather[0].WeatherText}</p>
+                            <p className="text-6xl">{cityCurrentWeather[0].Temperature.Metric.Value} {cityCurrentWeather[0].Temperature.Metric.Unit}</p>
+                            <p className="text-5xl">{cityCurrentWeather[0].WeatherText}</p>
                         </div>
                     </div>
-                    <div className="flex flex-col">
-                        <p className="text-4xl">{city}</p>
-                        <div>
-                            <p className="text-3xl">{getTime(cityCurrentWeather[0].EpochTime)}</p>
-                            <p className="text-3xl">{getDayOfWeek(cityCurrentWeather[0].EpochTime)}</p>
-                            <p className="text-3xl">{getDateNow(cityCurrentWeather[0].EpochTime)}</p>
+                    <div className="flex flex-col justify-between text-right pr-4 py-2">
+                        <p className="text-5xl">{city}</p>
+                        <div className="flex flex-col">
+                            <p className="text-4xl">{getTime(cityCurrentWeather[0].LocalObservationDateTime)}</p>
+                            <p className="text-4xl">{getDayOfWeek(cityCurrentWeather[0].LocalObservationDateTime)}</p>
+                            <p className="text-4xl">{getDateNow(cityCurrentWeather[0].LocalObservationDateTime)}</p>
                         </div>
                     </div>
                 </div>
-                <div className=" h-full bg-gray-800 w-1/2 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-25 border border-white border-opacity-25">
-                    <p className="text-xl">{getTime(sunForecast.DailyForecasts[0].Sun.Set)}</p>
-                    <p className="text-xl">{getTime(sunForecast.DailyForecasts[0].Sun.Rise)}</p>
-                    <p className="text-xl">{cityCurrentWeather[0].RelativeHumidity + '%'}</p>
+                <div className="flex flex-col h-full bg-white w-1/2 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-40 border border-white border-opacity-60 gap-8">
+                    <div className="flex justify-between px-4 py-2">
+                        <div className="flex flex-col items-center">
+                            <p className="text-3xl">UV-Index</p>
+                            <img src={uvIndex} className="w-20" />
+                            <p className="text-3xl">{cityCurrentWeather[0].UVIndex} - {cityCurrentWeather[0].UVIndexText}</p>
+                        </div>
+                        
+                        <div className="flex flex-col justify-center gap-4">
+                            <p className="text-3xl">Wind Speed</p>
+                            <p className="text-2xl">{cityCurrentWeather[0].Wind.Speed.Metric.Value + ' ' + cityCurrentWeather[0].Wind.Speed.Metric.Unit}</p>
+                        </div>
+                        <div className="flex flex-col justify-center gap-4">
+                            <p className="text-3xl">Humidity</p>
+                            <p className="text-2xl">{cityCurrentWeather[0].RelativeHumidity + '%'}</p>
+                        </div>
+
+                    </div>
+                    <div className="flex">
+                        <p className="text-xl">{getTime(sunForecast.DailyForecasts[0].Sun.Set)}</p>
+                        <p className="text-xl">{getTime(sunForecast.DailyForecasts[0].Sun.Rise)}</p>
+                    </div>
                 </div>
             </div>
             : <></>
